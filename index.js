8d5744b96e5911c23137e3fecd2cedcd8219f8a0
Victoria <vitalina27@gmail.com>
2017-11-06 09:25:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-
-  return  "I can\'t hear you!";
+  if(string===string.toLowerCase()){
+    return  "I can\'t hear you!";
+  }
+else if(string ===string.toUpperCase()){
+  return "YES INDEED!";
+}else {return "I love you, too.";}
+  
 }
