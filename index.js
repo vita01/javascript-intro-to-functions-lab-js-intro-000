740ed6e0849d49d153807e73ea9de7e1dfc1ee9f
Victoria <vitalina27@gmail.com>
2017-11-06 09:09:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ function shout(string){
 
   return string.toUpperCase();
 }
+functiun whisper(string){
+
+  return string.toLowerCase();
+}
+function logShout(string){
+return cobsole.log(string);
+  
+}
+
+function logWhisper(string){
+
+  return cobsole.log(string);
+}
+
+function sayHiToGrandma(string){
+
+  return string;
+}
